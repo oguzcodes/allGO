42d9a343f22da0c327e4c6a4eb98ddacdb7a2d0e
oguzcodes <121609568+oguzcodes@users.noreply.github.com>
2024-06-08 14:19:44 +0300

    n.th element of fibonacci number is found by using fibonacci function. an example of dynamic programming.

---
+++ FibonacciNumber/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type fibonacci struct {
+	n1 uint64
+	n2 uint64
+}
+
+func (fib *fibonacci) fibonacciNumber(n int) uint64 {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	}
+
+	fib.n1 = 0
+	fib.n2 = 1
+	for i := 3; i <= n; i++ {
+		if i%2 == 1 {
+			fib.n1 = fib.n1 + fib.n2
+		} else {
+			fib.n2 = fib.n1 + fib.n2
+		}
+	}
+
+	if n%2 == 1 {
+		return fib.n1
+	} else {
+		return fib.n2
+	}
+}
+
+func main() {
+	var a fibonacci
+	fmt.Printf("Fibonacci 8 = %d \n", a.fibonacciNumber(8))
+}
