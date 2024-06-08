6d3dc4d37223af08b900ed2823917918d4554781
oguzcodes <121609568+oguzcodes@users.noreply.github.com>
2024-06-08 14:52:53 +0300

    faster mod operation

--- FibonacciNumber/main.go
+++ FibonacciNumber/main.go
@@ -17,14 +17,14 @@ func (fib *fibonacci) fibonacciNumber(n int) uint64 {
 	fib.n1 = 0
 	fib.n2 = 1
 	for i := 3; i <= n; i++ {
-		if i%2 == 1 {
+		if i&1 == 1 {
 			fib.n1 = fib.n1 + fib.n2
 		} else {
 			fib.n2 = fib.n1 + fib.n2
 		}
 	}
 
-	if n%2 == 1 {
+	if n&1 == 1 {
 		return fib.n1
 	} else {
 		return fib.n2
